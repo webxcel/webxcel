7eedd78f88b0ac0fcf971a181d203945500f6b0f
joshua <oladokunjoshua2016@gmail.com>
2022-02-05 14:59:54 +0100

    fix vercel 10

--- vercel.sh
+++ vercel.sh
@@ -23,7 +23,7 @@ cp -a dist/client/. .output/static
 # Step 5: Bundle render function with it's depdendencies to the single javascript file
 # If you are using typescript, simply replace extension with ".ts"
 mkdir -p .output/server/pages
-yarn ncc build vercel/render.js --minify --out .output/server/pages
+yarn ncc build vercel/render.ts --minify --out .output/server/pages
 
 # Step 6: Make render function run on every request (catch all)
 cat >.output/routes-manifest.json <<EOF
